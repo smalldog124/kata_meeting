2da9536e498416aff6ec3fb1d78706264625714b
smalldog124 <panumars124@gmail.com>
2019-02-19 23:19:08 +0700

    [Aeded]:เพิ่มฟังก์ชัน GetMeeting

--- meeting.go
+++ meeting.go
@@ -2,8 +2,20 @@ package meeting
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 )
+
+func GetMeeting(text string) string {
+	people := strings.Split(text, ";")
+	format := Formating(people)
+	var textUpper []string
+	for _, name := range format {
+		textUpper = append(textUpper, UpperCase(name))
+	}
+	sort.Strings(textUpper)
+	return fmt.Sprintf("%s%s%s%s%s%s%s", textUpper[0], textUpper[1], textUpper[2], textUpper[3], textUpper[4], textUpper[5], textUpper[6])
+}
 
 func UpperCase(text string) string {
 	return strings.ToUpper(text)
@@ -11,7 +23,6 @@ func UpperCase(text string) string {
 
 func Formating(text []string) []string {
 	var s []string
-
 	for _, people := range text {
 		name := strings.Split(people, ":")
 		format := fmt.Sprintf("(%s, %s)", name[1], name[0])
--- meeting_test.go
+++ meeting_test.go
@@ -16,6 +16,15 @@ func Test_UpperCase_Input_Small_Should_Be_SMALL(t *testing.T) {
 	assert.Equal(t, expected, actual)
 }
 
+func Test_Split_Input_Fred_Corwill_Should_Be_Fred_Coreill(t *testing.T) {
+	expected := "(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
+	text := "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill"
+
+	actual := meeting.GetMeeting(text)
+
+	assert.Equal(t, expected, actual)
+}
+
 func Test_Fomating_Input_List_Name_Shoud_Be_Formater(t *testing.T) {
 	expected := []string{"(Corwill, Fred)", "(Corwill, Wilfred)", "(Tornbull, Barney)", "(Tornbull, Betty)", "(Tornbull, Bjon)", "(Corwill, Raphael)", "(Corwill, Alfred)"}
 	text := []string{"Fred:Corwill", "Wilfred:Corwill", "Barney:Tornbull", "Betty:Tornbull", "Bjon:Tornbull", "Raphael:Corwill", "Alfred:Corwill"}
