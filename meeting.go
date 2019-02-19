bbae3ddac51ec1eda12195883ff729cc1def6a01
smalldog124 <panumars124@gmail.com>
2019-02-19 22:55:40 +0700

    [Added]:เพิ่มฟังก์ชัน Formating

--- meeting.go
+++ meeting.go
@@ -1,9 +1,21 @@
 package meeting
 
 import (
+	"fmt"
 	"strings"
 )
 
 func UpperCase(text string) string {
 	return strings.ToUpper(text)
+}
+
+func Formating(text []string) []string {
+	var s []string
+
+	for _, people := range text {
+		name := strings.Split(people, ":")
+		format := fmt.Sprintf("(%s, %s)", name[1], name[0])
+		s = append(s, format)
+	}
+	return s
 }
--- meeting_test.go
+++ meeting_test.go
@@ -15,3 +15,12 @@ func Test_UpperCase_Input_Small_Should_Be_SMALL(t *testing.T) {
 
 	assert.Equal(t, expected, actual)
 }
+
+func Test_Fomating_Input_List_Name_Shoud_Be_Formater(t *testing.T) {
+	expected := []string{"(Corwill, Fred)", "(Corwill, Wilfred)", "(Tornbull, Barney)", "(Tornbull, Betty)", "(Tornbull, Bjon)", "(Corwill, Raphael)", "(Corwill, Alfred)"}
+	text := []string{"Fred:Corwill", "Wilfred:Corwill", "Barney:Tornbull", "Betty:Tornbull", "Bjon:Tornbull", "Raphael:Corwill", "Alfred:Corwill"}
+
+	actual := meeting.Formating(text)
+
+	assert.Equal(t, expected, actual)
+}
